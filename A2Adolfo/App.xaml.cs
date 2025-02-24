f57d18bc0184b7bbe74674cb68c6786f94f546cb
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-02-23 22:35:07 -0500

    Initial commit

---
+++ A2Adolfo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace A2Adolfo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ A2Adolfo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace A2Adolfo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
