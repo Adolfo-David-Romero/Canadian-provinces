4d6e4ae81a03646fe5fbe61765e8b301c05aa251
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-02-23 22:42:28 -0500

    Added empty second page

--- A2Adolfo/Platforms/MacCatalyst/AppDelegate.cs
+++ A2Adolfo/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace A2Adolfo;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
